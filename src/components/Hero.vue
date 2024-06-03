f1c296b613a8b455eab658e1768fec5720c98091
Reza Jaber <frontendrezajaber@gmail.com>
2024-06-03 21:40:52 +0200

    responsive: Hero

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -2,17 +2,23 @@
 
 <template>
   <div class="relative">
-    <img class="h-auto w-full" src="../assets/img/hero.jpeg" alt="Hero Image" />
+    <img
+      class="h-[500px] w-full transform object-cover transition-transform duration-500 ease-in-out hover:scale-105 md:h-auto lg:h-[700px] 2xl:h-[800px]"
+      src="../assets/img/hero.jpeg"
+      alt="Hero Image"
+    />
     <div class="absolute inset-0 bg-sky-400 opacity-30"></div>
     <div class="absolute inset-0 bg-black opacity-35"></div>
 
     <div class="absolute inset-0 flex items-center justify-center p-4 text-center text-white">
-      <div class="grid gap-5">
-        <h3 class="mb-2 text-xl font-semibold">YOUR HOME, YOUR HEAVEN</h3>
-        <h1 class="mb-4 text-7xl font-semibold">CLEAN YOUR WAY TO <br />YOUR HAPPINESS</h1>
-        <h6 class="text-lg">
+      <div class="grid md:gap-5">
+        <h3 class="mb-2 font-semibold sm:text-lg md:text-xl">YOUR HOME, YOUR HEAVEN</h3>
+        <h1 class="mb-4 text-4xl font-semibold sm:text-6xl md:text-7xl">
+          CLEAN YOUR WAY TO <br />YOUR HAPPINESS
+        </h1>
+        <h6 class="sm:text-md text-sm md:text-lg">
           Classical Latin literature from enter, making it over kusta years old. Richard McClintock
-          <br />a Latin professor at hampden sydney College in Virginia
+          <br class="hidden md:block" />a Latin professor at hampden sydney College in Virginia
         </h6>
       </div>
     </div>
