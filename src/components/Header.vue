3399cc9db8ba11ca02546f717fdf0253f90e8bae
Reza Jaber <frontendrezajaber@gmail.com>
2024-06-03 21:40:34 +0200

    responsive: Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,24 +6,24 @@ import { Phone, Mail, MapPin, Facebook, Twitter, Instagram, Linkedin } from 'luc
   <div>
     <div class="mx-auto flex max-w-[1080px] items-start justify-between">
       <!-- HEADER TABLET & ABOVE -->
-      <div class="mt-2.5 flex items-center gap-4">
+      <div class="mt-2.5 flex items-center gap-5">
         <div class="flex items-center gap-2 text-white">
           <Phone class="w-5 fill-white stroke-none" />
-          <span>+123 456 7890</span>
+          <span class="hidden md:block">+123 456 7890</span>
         </div>
 
-        <span class="font-extralight text-white opacity-80">|</span>
+        <span class="hidden font-extralight text-white opacity-80 md:block">|</span>
 
         <div class="flex items-center gap-2 text-white">
           <Mail class="stroke-header w-5 fill-white" />
-          <span>+123 456 7890</span>
+          <span class="hidden md:block">+123 456 7890</span>
         </div>
 
-        <span class="font-extralight text-white opacity-80">|</span>
+        <span class="hidden font-extralight text-white opacity-80 md:block">|</span>
 
         <div class="flex items-center gap-2 text-white">
           <MapPin class="w-5" />
-          <span>+123 456 7890</span>
+          <span class="hidden md:block">+123 456 7890</span>
         </div>
       </div>
 
