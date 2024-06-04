67993806d9d12aa33b94ff561337efe5fc4a4422
Reza Jaber <frontendrezajaber@gmail.com>
2024-06-04 13:26:05 +0200

    change: transition App

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,20 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <RouterView />
+  <Transition>
+    <RouterView />
+  </Transition>
 </template>
+
+<style scoped>
+/* we will explain what these classes do next! */
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 1s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
